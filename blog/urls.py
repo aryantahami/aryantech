f84e8d8eb72457d8470ea589a887aa45fb38c51c
aryan <aryancomp0106@gmail.com>
2023-05-16 11:03:53 +0430

    add blog app

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.myhttp),
+    path('test/', views.myblog, name="blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
-def myhttp(request):
-    return HttpResponse("hello aryan")
+def myblog(request):
+    return render(request, 'blog/blog.html')
